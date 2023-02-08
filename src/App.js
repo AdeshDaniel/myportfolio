6cbd6c61b62db3a9152d9791e291eda12bbe3713
AdeshDaniel <stockhutfxcompany@gmail.com>
2023-02-08 15:56:32 +0100

    done with the boiler plate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { About, Header, Footer, Skills, Work, Testimonial } from "./container";
+import { Navbar } from "./components";
 
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
